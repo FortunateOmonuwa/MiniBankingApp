19e3398f090f214512c8a31c011d10a9263d268b
fortunate3d <omonuwafortunate@gmail.com>
2022-11-09 10:51:32 +0100

    Initial Commit

---
+++ MethodExtensionExercise/MethodExtensionExercise/MethodExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Globalization;
+
+public static class MethodExtension
+{
+    public static string ExtensionMethod(this Decimal number)
+    {
+         number = 180.2m;
+        return number.ToString("C2",CultureInfo.CurrentCulture);
+        
+
+    }
+}
---
+++ MethodExtensionExercise/MethodExtensionExercise/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+Decimal result = new Decimal();
+var newResult = result.ExtensionMethod();
+Console.WriteLine(newResult);
+
